0968473b97030dc9aca57638839f6db4a066a6bf
yhd03 <younesshida03@gmail.com>
2026-02-22 22:17:44 +0100

    Create hw2f_model.py

---
+++ src/hw2f_model.py
@@ -0,0 +1,8 @@
+class HW2FModel:
+    def __init__(self, parameters):
+        # TODO: Implement HW2F model initialization
+        pass
+
+    def simulate_paths(self, t, n_paths):
+        # TODO: Implement short rate simulation
+        pass
